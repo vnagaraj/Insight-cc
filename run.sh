dfdf79795291c7ed4ebd33ec8e45e9905d82a3f9
Vivek Nagarajan <vivekn@gmail.com>
2016-04-03 09:26:31 -0700

    Updated run script

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the average_degree calculation
+# example of the run script for running the word count
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
+# I'll launch my programs, with the input directory tweet_input and output the files in the directory tweet_output
+#python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
+
+
+
+cd src
+javac launch/Executor.java
+java -cp $CLASSPATH launch.Executor ../tweet_input/tweets.txt ../tweet_output/output.txt
+
 
 
 
